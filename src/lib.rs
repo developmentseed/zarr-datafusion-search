4b8a78f70e4583b38c421ff076d299636e8dcc92
Kyle Barron <kyle@developmentseed.org>
2025-10-14 16:00:35 -0400

    Remove unused dependencies

--- src/lib.rs
+++ src/lib.rs
@@ -1,33 +1,7 @@
+#![cfg_attr(docsrs, feature(doc_cfg))]
+#![cfg_attr(not(test), warn(unused_crate_dependencies))]
+
 pub mod error;
 pub mod table_provider;
 #[cfg(test)]
 pub mod testing;
-
-// pub use file_format::{ZarrMetaFormat, ZarrMetaFormatFactory};
-
-// #[cfg(test)]
-// mod tests {
-//     use std::env::current_dir;
-//     use std::sync::Arc;
-
-//     use datafusion::execution::SessionStateBuilder;
-//     use datafusion::prelude::SessionContext;
-
-//     use super::*;
-
-//     #[tokio::test]
-//     async fn test_geoparquet() {
-//         let file_format = Arc::new(ZarrMetaFormatFactory::default());
-//         let state = SessionStateBuilder::new()
-//             .with_file_formats(vec![file_format])
-//             .build();
-//         let ctx = SessionContext::new_with_state(state).enable_url_table();
-
-//         dbg!(current_dir().unwrap());
-//         let df = ctx
-//             .sql("SELECT * FROM 'data/zarr_store.zarr' as table")
-//             .await
-//             .unwrap();
-//         df.show().await.unwrap();
-//     }
-// }
