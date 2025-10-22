e8b8fdc7a0851d013707d552895285bcb4c87450
Kyle Barron <kyle@developmentseed.org>
2025-10-22 17:43:49 -0400

    fix table construction

--- python/src/table.rs
+++ python/src/table.rs
@@ -2,6 +2,7 @@ use std::sync::Arc;
 
 use datafusion_ffi::table_provider::FFI_TableProvider;
 use pyo3::prelude::*;
+use pyo3::pybacked::PyBackedStr;
 use pyo3::types::PyCapsule;
 use zarr_datafusion_search::table_provider::ZarrTableProvider;
 
@@ -11,13 +12,14 @@ pub struct PyZarrTable(Arc<ZarrTableProvider>);
 #[pymethods]
 impl PyZarrTable {
     #[new]
-    pub fn new(zarr_path: String) -> PyResult<Self> {
-        let table_provider = ZarrTableProvider::new_filesystem(zarr_path).map_err(|e| {
-            PyErr::new::<pyo3::exceptions::PyRuntimeError, _>(format!(
-                "Failed to create ZarrTableProvider: {}",
-                e
-            ))
-        })?;
+    pub fn new(zarr_path: String, group_path: PyBackedStr) -> PyResult<Self> {
+        let table_provider =
+            ZarrTableProvider::new_filesystem(zarr_path, &group_path).map_err(|e| {
+                PyErr::new::<pyo3::exceptions::PyRuntimeError, _>(format!(
+                    "Failed to create ZarrTableProvider: {}",
+                    e
+                ))
+            })?;
         Ok(PyZarrTable(Arc::new(table_provider)))
     }
 
