7dae2d1e3240ae2f760d0572980bceed147b8dfb
sharkinsspatial <sharkinsgis@gmail.com>
2026-03-17 11:41:46 -0400

    Export full API from Python module.

--- python/python/zarr_datafusion_search/__init__.py
+++ python/python/zarr_datafusion_search/__init__.py
@@ -1,6 +1,8 @@
 from __future__ import annotations
 
-from ._rust import * # noqa: F403
+from ._rust import ZarrTable
 from ._rust import ___version
 
 __version__: str = ___version()
+
+__all__ = ["ZarrTable", "__version__"]
